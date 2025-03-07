d366f4d60b1794585044dffc65205ef17ef17f3d
alifrayhan <alifrayhan92@gmail.com>
2025-03-07 21:20:11 +0600

    page not found

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          plusJakarta: ['Plus Jakarta Sans', 'sans-serif'],
+          playfair: ['Playfair Display', 'serif'],
+          inter: ['Inter', 'sans-serif'],
+          poppins: ['Poppins', 'sans-serif'],
+        },
+        colors: {
+          primary: '#094B72',
+          secondary: {
+            100: '#F9F8F9',
+            500: '#8B8C8C',
+            600: '#4F4F4F',
+            700: '#2D2F30',
+            800: '#151617',
+          },
+          tertiary: {
+            100: '#F4F5F7',
+            200: '#EEEFF4',
+            300: '#D9DBE1',
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
